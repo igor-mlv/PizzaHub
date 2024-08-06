ba3fe5f816568a8a2f9edc3fac129ac29c9224f2
igor-mlv <malov7704@gmail.com>
2024-08-05 21:42:49 -0400

    added product card element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Container, Filters, Title, TopBar } from "@/components/shared";
+import { ProductCard } from "@/components/shared/product-card";
 
 export default function Home() {
   return (
@@ -20,7 +21,7 @@ export default function Home() {
           {/* right panel - list of products */}
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              List of items
+              <ProductCard id={0} name="Pizza Hawaii" price={22} imageUrl="https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg" />
             </div>
           </div>
         </div>
