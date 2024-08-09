b8a89b7a9da77b32a45bc662a0e82afa62c7c61d
igor-mlv <malov7704@gmail.com>
2024-08-09 15:32:44 -0400

    refactoring for product/id page

--- src/app/(root)/product/[id]/page.tsx
+++ src/app/(root)/product/[id]/page.tsx
@@ -1,43 +1,32 @@
-
-import { Container, GroupVariants, PizzaImage, Title } from "@/shared/components/shared";
-import { prisma } from "@/prisma/prisma-client"
-import { notFound } from "next/navigation";
+import { Container, ProductForm } from '@/shared/components/shared';
+import { prisma } from '@/prisma/prisma-client';
+import { notFound } from 'next/navigation';
 
 export default async function ProductPage({ params: { id } }: { params: { id: string } }) {
-    const product = await prisma.product.findFirst({ where: { id: Number(id) } });
+    const product = await prisma.product.findFirst({
+        where: { id: Number(id) },
+        include: {
+            ingredients: true,
+            category: {
+                include: {
+                    products: {
+                        include: {
+                            variants: true,
+                        },
+                    },
+                },
+            },
+            variants: true,
+        },
+    });
 
     if (!product) {
         return notFound();
     }
 
     return (
-        <Container className="flex- flex-col my-10">
-            <div className="flex flex-1 ">
-                <PizzaImage imageUrl={product.imageUrl} size={20} />
-
-                <div className="w-[490px] bg-[#FCFCFC] p-7">
-                    <Title text={product.name} size="md" className="font-extrabold mb-1" />
-
-                    <p className="text-gray-400">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-
-                    <GroupVariants
-                        items={[
-                            {
-                                name: "Small",
-                                value: '1'
-                            },
-                            {
-                                name: "Medium",
-                                value: '2'
-                            },
-                            {
-                                name: "Large",
-                                value: '3'
-                            },
-                        ]} />
-                </div>
-            </div>
-
+        <Container className="flex flex-col my-10">
+            <ProductForm product={product} />
         </Container>
-    )
+    );
 }
