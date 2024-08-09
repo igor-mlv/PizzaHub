01e0c2bf83eb83551c8ffa1b913f39be2887ed03
igor-mlv <malov7704@gmail.com>
2024-08-09 12:55:50 -0400

    set up layout and functionality for user cart

--- src/shared/components/shared/index.ts
+++ src/shared/components/shared/index.ts
@@ -21,4 +21,5 @@ export { CartDrawer } from './cart-drawer'
 export { CartDrawerItem } from './cart-drawer-item'
 export { CountButton } from './count-button'
 export { CountIconButton } from './count-icon-button'
+export { ProductForm } from './product-form'
 export * from './modals';
--- src/shared/components/shared/modals/choose-product-modal.tsx
+++ src/shared/components/shared/modals/choose-product-modal.tsx
@@ -4,11 +4,8 @@ import { Dialog, DialogContent } from '@/shared/components/ui/dialog';
 import { cn } from '@/shared/lib/utils';
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import { ChooseProductForm, ChoosePizzaForm } from '..';
 import { ProductWithRelations } from '@/@types/prisma';
-// import { useCartStore } from '@/shared/store';
-// import toast from 'react-hot-toast';
-// import { ProductForm } from '../product-form';
+import { ProductForm } from '../';
 
 interface Props {
     product: ProductWithRelations;
@@ -17,7 +14,7 @@ interface Props {
 
 export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
     const router = useRouter();
-    const isPizzaForm = Boolean(product.variants[0].pizzaType);
+
     return (
         <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
             <DialogContent
@@ -25,28 +22,7 @@ export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
                     'p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden',
                     className,
                 )}>
-                {
-                    isPizzaForm ? (
-                        <ChoosePizzaForm
-                            imageUrl={product.imageUrl}
-                            name={product.name}
-                            ingredients={product.ingredients}
-                            variants={product.variants}
-                            onSubmit={function (itemId: number, ingredients: number[]): void {
-                                throw new Error('Function not implemented.');
-                            }} />
-                    ) : (
-                        <ChooseProductForm
-                            imageUrl={product.imageUrl}
-                            name={product.name}
-                            ingredients={[]}
-                            variants={[]}
-                            onSubmit={function (itemId: number, ingredients: number[]): void {
-                                throw new Error('Function not implemented.');
-                            }} />
-                    )
-                }
-                {/* <ProductForm product={product} onSubmit={() => router.back()} /> */}
+                <ProductForm product={product} onSubmit={() => router.back()} />
             </DialogContent>
         </Dialog>
     );
