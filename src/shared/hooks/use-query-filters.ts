92b908e8be5efd39d649dd1505809d6f4ace57c3
igor-mlv <malov7704@gmail.com>
2024-08-14 09:07:10 -0400

    fixed issue with query url

--- src/shared/hooks/use-query-filters.ts
+++ src/shared/hooks/use-query-filters.ts
@@ -5,22 +5,26 @@ import { useRouter } from "next/navigation";
 
 export const useQueryFilters = (filters: Filters) => {
     const router = useRouter();
+    const isMounted = React.useRef(false);
 
     React.useEffect(() => {
-        const params = {
-            ...filters.price,
-            pizzaTypes: Array.from(filters.selectedPizzaTypes),
-            sizes: Array.from(filters.selectedSizes),
-            ingredients: Array.from(filters.selectedIngredients)
+
+        if (isMounted.current) {
+            const params = {
+                ...filters.price,
+                pizzaTypes: Array.from(filters.selectedPizzaTypes),
+                sizes: Array.from(filters.selectedSizes),
+                ingredients: Array.from(filters.selectedIngredients)
+            }
+
+            const query = qs.stringify(params, {
+                arrayFormat: 'comma'
+            })
+
+            router.push(`?${query}`, {
+                scroll: false,
+            });
         }
-
-        const query = qs.stringify(params, {
-            arrayFormat: 'comma'
-        })
-
-        router.push(`?${query}`, {
-            scroll: false,
-        });
-
+        isMounted.current = true;
     }, [filters])
 }
